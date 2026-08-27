254fe60256201913b57e410b16dd6f85f47baf0e
Ritik <ritik@example.com>
2026-08-27 12:49:15 +0530

    Remove migrations property from prisma config

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -5,13 +5,10 @@ import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  datasource: {
+  datasources: {
     db: {
       provider: "postgresql",
       url: process.env.DATABASE_URL
     }
-  },
-  migrations: {
-    path: "prisma/migrations",
   }
 });
